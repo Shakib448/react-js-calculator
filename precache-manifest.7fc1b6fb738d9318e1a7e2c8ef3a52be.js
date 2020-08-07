8e98c3d9df8752235a0bf8f98b4c2a817ea645ea
Shakib448 <shakiba448@gmail.com>
2020-08-07 19:42:10 +0600

    Updates

---
+++ precache-manifest.7fc1b6fb738d9318e1a7e2c8ef3a52be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84dc3a315762b287edf7c58c061a3b18",
+    "url": "/react-js-calculator/index.html"
+  },
+  {
+    "revision": "f592aaf99d4be2f9ab58",
+    "url": "/react-js-calculator/static/css/main.e5c9dcb6.chunk.css"
+  },
+  {
+    "revision": "6eadf8749f6af9afe675",
+    "url": "/react-js-calculator/static/js/2.a6c44453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-calculator/static/js/2.a6c44453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f592aaf99d4be2f9ab58",
+    "url": "/react-js-calculator/static/js/main.65ef0d62.chunk.js"
+  },
+  {
+    "revision": "9179f07da7db46a60b16",
+    "url": "/react-js-calculator/static/js/runtime-main.1a74dc99.js"
+  }
+]);
